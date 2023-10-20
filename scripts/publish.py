fa6c1215b6765598c3c171a9aa94cbb7d83f0660
rafael-abuawad <rafael.abuawad@live.com>
2023-10-20 17:27:20 -0400

    Fixed verify script

--- scripts/publish.py
+++ scripts/publish.py
@@ -2,5 +2,5 @@ from ape import networks
 
 def main():
     etherscan = networks.provider.network.explorer
-etherscan.publish_contract("0x2993276bd267b6fb73f99976b70cd3a06d549691")
-etherscan.publish_contract("0x9aae4c9d1e3efff3c20ff7b7a9fa9f017c3b1c86")
+    etherscan.publish_contract("0x2993276bd267b6fb73f99976b70cd3a06d549691")
+    etherscan.publish_contract("0x9aae4c9d1e3efff3c20ff7b7a9fa9f017c3b1c86")
